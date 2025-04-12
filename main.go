6e6d969a05e9bf1cfd59f9a0ed7f9dd417d79760
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-04-13 00:10:44 +0600

    Printiing a message

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
-package main
+package main //  main package is the entry point of the program
 
-import "fmt"
+import "fmt" // Importing the fmt package for formatted I/O
 
-func main(){
-	fmt.Println("Hello, World!")
+func main(){ // main function is the entry point of the program
+	fmt.Println("Hello, World!") // Print "Hello, World!" to the console
 }
