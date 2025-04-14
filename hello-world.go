facc1c0d3a2402b0767afc7ec33cb591dc2ab3ea
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-04-15 02:23:39 +0600

    Learn about GO.PrintF

--- hello-world.go
+++ hello-world.go
@@ -4,34 +4,27 @@ import (
 	"fmt"
 )
 
-// This type of declaration is not allowed in the package scope
-//c:= 3.14
-
-// allowed in the package scope
-var d int = 5 
-var e = 3.14 
-var f int
-var a, b, c int = 1, 3, 5
-
-var (
-    myNum int = 10
-    //newNum := 20
-    // var myString string = "Hello, World!"
-    myString = "Hello, World!"
-    myFloat float64 = 3.14
-)
-
 func main() {
-    var a string;
-    a = "Learning GO is fun!"
-    b := 42 // this is allowed in the function scope. also we must put a value to it.   
-    i,b := "--",100 // this is allowed in the function scope. also we must put a value to it.
+    /*
+    var myString string = "learing about GO!"
+    var myInt int = 12
 
-    randomTexts := "This is a random text"
+    ////   PrnitF is a function that formats and prints its arguments to the standard output.
 
-    fmt.Println(randomTexts, " ", myNum, " ", myString, " ", myFloat, " ", a, " ", b, " ", i, " ", c, " ", d, " ", e, " ", f)
-    // fmt.Println(a)
-    // fmt.Println(b)
-    // fmt.Println(i)
+    // The %v verb is used to print the value in a default format.
+    fmt.Printf("%v\n", myInt)
+    fmt.Printf("%v\n", myString)
 
+    // The %#v verb is used to print the value in Go syntax.
+    fmt.Printf("%#v\n", myInt)
+    fmt.Printf("%#v\n", myString)
+
+    // The %T verb is used to print the type of the value.
+    fmt.Printf("%T\n", myInt)
+    fmt.Printf("%T\n", myString)
+*/
+
+    type myType int
+    var myVar myType = 42
+    fmt.Printf("%T\n", myVar)
 }
