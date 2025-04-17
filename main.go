0df91353ed631744650b5f7ff34e001223a455ec
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-04-17 07:56:53 +0600

    beautiful int array example

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := getArrayLength()
+	// displayArray(arr)
+	getArrayValues(arr)
+	displayArray(arr)
+}
+
+func getArrayLength() []int {
+	var arrLen int
+	fmt.Print("Enter the length of the array: ")
+	fmt.Scan(&arrLen)
+	arr := make([]int, arrLen)
+	return arr
+}
+
+func getArrayValues(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Print("Enter value for element ", i, ": ")
+		fmt.Scan(&arr[i])
+	}
+}
+
+func displayArray(arr []int) {
+	fmt.Println("The array is:", arr)
+	fmt.Println("The length of the array is:", len(arr))
+	fmt.Println("The capacity of the array is:", cap(arr))
+	fmt.Println("The address of the array is:", &arr)
+	fmt.Println("The address of the first element is:", &arr[0])
+	fmt.Println("---------------------------------------------")
 }
